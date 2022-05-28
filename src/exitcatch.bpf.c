7da4c871be885971dc8499720c03172fdbbb41e2
hrpccs <hrpccs@foxmail.com>
2022-05-28 14:12:38 +0800

    add stacktrace

--- src/exitcatch.bpf.c
+++ src/exitcatch.bpf.c
@@ -4,11 +4,24 @@
 #include <bpf/bpf_core_read.h>
 
 #define TASK_COMM_LEN 16
+#define MAX_STACK_DEPTH 10
 
-SEC("tracepoint/sched/sched_process_exit") // the path in the debugfs
-int tracepoint__sched__sched_process_exit(void *ctx)
-{
-	struct {
+//Use to store stacktrace and will be used by bpf_get_stackid()
+struct bpf_map_def SEC("maps") map_stack_traces = {
+	.type = BPF_MAP_TYPE_STACK_TRACE,
+	.key_size = sizeof(u32),
+	.value_size = sizeof(size_t) * MAX_STACK_DEPTH,
+	.max_entries = 8192
+};
+
+//Use a ringbuffer Map to send data down to userspace 
+struct bpf_map_def SEC("maps") rb = {
+	.type = BPF_MAP_TYPE_RINGBUF,
+	.max_entries = 256 * 1024,
+};
+
+//Define the format of the event to send to userspace
+struct event{
 		__u64 start_time;
 		__u64 exit_time;
 		__u32 pid;
@@ -17,27 +30,38 @@ int tracepoint__sched__sched_process_exit(void *ctx)
 		__u32 sig;
 		int exit_code;
 		char comm[TASK_COMM_LEN];
-	}event;
-	__u64 pid = bpf_get_current_pid_tgid();
-	__u32 tid = (__u32)pid;
-	pid = pid >> 32; 
+		unsigned long stack_id;
+};
+
+
+SEC("tracepoint/sched/sched_process_exit") // the path in the debugfs, and debugfs need to be mounted
+int tracepoint__sched__sched_process_exit(void *ctx)
+{
 	struct task_struct *task;
+	struct event *e;
+
+	u64 pid = bpf_get_current_pid_tgid();
+	u32 tid = (u32)pid;
+	pid = pid >> 32; 
 	// get information in task_struct
-	// exit_code,
 	task = (struct task_struct *)bpf_get_current_task();
 	int exitcode = BPF_CORE_READ(task,exit_code);
+
 	if(exitcode == 0){ //exit normally
 		return 0;
 	}
-	event.exit_code = exitcode >> 8;
-	event.sig = exitcode & 0xff; 
-	event.start_time = BPF_CORE_READ(task,start_time);
-	event.ppid = BPF_CORE_READ(task,parent,pid);
-	bpf_get_current_comm(&(event.comm),TASK_COMM_LEN);
-	event.exit_time = bpf_ktime_get_ns();
+	//Log event to ringbuffer to be read by userspace 
+	e = bpf_ringbuf_reserve(&rb,sizeof(*e), 0);
+	if(e){
+		e->stack_id = bpf_get_stackid(ctx,&map_stack_traces,0);
+		e->exit_code = exitcode >> 8;
+		e->sig = exitcode & 0xff;
+		e->start_time = BPF_CORE_READ(task,start_time);
+		e->ppid = BPF_CORE_READ(task,parent,pid);
+		e->exit_time = bpf_ktime_get_ns();
+		bpf_get_current_comm(&(e->comm),TASK_COMM_LEN);
+	}
 
-	char str[] = "comm:%s exitcode:%d signal:%d\n";
-	bpf_trace_printk(str,sizeof(str),event.comm,event.exit_code,event.sig);
 	return 0;
 }
 char _license[] SEC("license") = "GPL";
