d1817cd2aca44c921c58cbda55a123278acc1df0
TangZh <909485276@qq.com>
2022-05-31 09:46:03 +0800

    Add some test programs and Add color to Log

---
+++ crashtest/multiThread_SegFault.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <omp.h>
+#include<color.h>
+int main()
+{
+// #pragma omp parallel for
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     int *ptr = (void *)main;
+    //     printf("attend to write read only mem %p", ptr);
+    //     *ptr = 1000;
+    // }
+    printf(NONE"Test\n"NONE);
+    printf(LIGHT_BLUE"Test\n"NONE);
+    printf(LIGHT_CYAN"Test\n"NONE);
+    printf(LIGHT_GRAY"Test\n"NONE);
+    printf(LIGHT_GREEN"Test\n"NONE);
+    printf(LIGHT_PURPLE"Test\n"NONE);
+    printf(LIGHT_RED"Test\n"NONE);
+    printf(RED"Test\n"NONE);
+    printf(GREEN"Test\n"NONE);
+    printf(DARY_GRAY"Test\n"NONE);
+    printf(BLUE"Test\n"NONE);
+    printf(CYAN"Test\n"NONE);
+    printf(PURPLE"Test\n"NONE);
+    printf(BROWN"Test\n"NONE);
+    printf(YELLOW"Test\n"NONE);
+    printf(WHITE"Test\n"NONE);
+    printf("Test\n");
+    return 0;
+}
