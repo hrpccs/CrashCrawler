5533fe3f855c0fb555ec6b34365d90d884f032d1
hrpccs <hrpccs@foxmail.com>
2022-06-03 23:31:57 +0800

    sync

--- src/exitcatch.bpf.c
+++ src/exitcatch.bpf.c
@@ -8,7 +8,7 @@
 //Use a ringbuffer Map to send data down to userspace 
 struct {
 	__uint(type,BPF_MAP_TYPE_RINGBUF);
-	__uint(max_entries,256 * 1024);
+	__uint(max_entries,64 * sizeof(struct event));
 }rb SEC(".maps");
 
 //Use to store stacktrace and will be used by bpf_get_stackid()
@@ -24,10 +24,16 @@ SEC("tp/sched/sched_process_exit")
 int trace_event_raw_sched_process_exit(struct trace_event_raw_sched_process *ctx)
 {
 	struct task_struct *task;
+	struct vm_area_struct *vma;
+	struct file *file;
 	struct event *e;
+
 
 	// get information in task_struct
 	task = (struct task_struct *)bpf_get_current_task();
+	vma = BPF_CORE_READ(task->mm->mmap)
+
+	long long temp = 0;
 	int exitcode = BPF_CORE_READ(task,exit_code);
 
 	if(exitcode == 0){ //exit normally
@@ -39,12 +45,27 @@ int trace_event_raw_sched_process_exit(struct trace_event_raw_sched_process *ctx
 		e->stack_id = bpf_get_stackid(ctx,&map_stack_traces,0);
 		e->exit_code = exitcode >> 8;
 		e->sig = exitcode & 0xff;
-		// e->pid = BPF_CORE_READ(task,pid);
-		// e->tid = BPF_CORE_READ(task,tgid);
 		e->pid = BPF_CORE_READ(task,tgid);
 		e->tid = BPF_CORE_READ(task,pid);
 		e->ppid = BPF_CORE_READ(task,parent,pid);
 		bpf_get_current_comm(&(e->comm),TASK_COMM_LEN);
+		
+		#pragma clang loop unroll(full)
+		for(int i=0;i<MAX_MMAP_ENTRY;i++){
+			if(!vma){
+				file = BPF_CORE_READ(vma->vm_file);
+				if(file){
+					e->mmap[i].dev = BPF_CORE_READ(vma->vm_file->f_inode->i_sb->s_dev);
+					e->mmap[i].ino = BPF_CORE_READ(vma->vm_file->f_inode->i_ino);
+					temp = BPF_CORE_READ(vma->vm_pgoff);
+					e->mmap[i].pgoff = temp << PAGE_SHIFT;
+				}
+				e->mmap[i].start = BPF_CORE_READ(vma->vm_start);
+				e->mmap[i].end = BPF_CORE_READ(vma->vm_end);
+				e->mmap[i].flags = BPF_CORE_READ(vma->vm_flags);
+				
+			}
+		}
 		bpf_ringbuf_submit(e,0);
 	}
 
--- src/exitcatch.h
+++ src/exitcatch.h
@@ -5,6 +5,26 @@
 #define TASK_COMM_LEN 16
 #define VALUESIZE  MAX_STACK_DEPTH * sizeof(size_t)
 
+#define MAX_VMA_ENTRY 50 
+#define MAXLEN_VMA_NAME 64
+#define PAGE_SHIFT 13 //8KB differs from kernels
+
+enum vma_type {
+	file,
+	
+}
+
+struct mmap_struct{
+	unsigned long start;
+	unsigned long end;
+	unsigned long flags;
+	unsigned long long pgoff;
+	unsigned long ino;
+	dev_t dev;
+	char name[MAXLEN_VMA_NAME]; //name
+	
+};
+
 struct event{
 		pid_t pid;
 		pid_t tid;
@@ -13,6 +33,7 @@ struct event{
 		int exit_code;
 		char comm[TASK_COMM_LEN];
 		unsigned long stack_id;
+		struct mmap_struct mmap[MAX_VMA_ENTRY];
 };
 
 
