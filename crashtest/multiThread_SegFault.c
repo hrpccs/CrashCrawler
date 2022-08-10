61c61238eaba540969f43f064f8779643026a52c
Tangzh33 <909485276@qq.com>
2022-08-10 13:38:44 +0800

    Add multistack test

--- crashtest/multiThread_SegFault.c
+++ crashtest/multiThread_SegFault.c
@@ -2,18 +2,30 @@
 #include <omp.h>
 #include<color.h>
 #include <sys/types.h>
-int main()
+void crashtest_3()
 {
-#pragma omp parallel for
+    // #pragma omp parallel for
     for (int i = 0; i < 4; i++)
     {
         printf("My Tid is % 8d\n", gettid());
-        int *ptr = (void *)main;
+        int *ptr = (void *)crashtest_3;
         // for (int i = 0; i < 100000000000000; i++);
         printf("attend to write read only mem %p\n", ptr);
         if(i == 3)
             *ptr = 1000;
     }
+}
+void crashtest_2()
+{
+    crashtest_3();
+}
+void crashtest_1()
+{
+    crashtest_2();
+}
+int main()
+{
+    crashtest_3();
     // printf(NONE"Test\n"NONE);
     // printf(LIGHT_BLUE"Test\n"NONE);
     // printf(LIGHT_CYAN"Test\n"NONE);
