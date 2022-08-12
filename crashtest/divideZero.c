50b8b2e43cb7d1d247fa480b0c9b60cf850e9a1c
Tangzh33 <909485276@qq.com>
2022-08-12 17:08:32 +0800

    Save before adding multiple testing cases.

---
+++ crashtest/divideZero.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <omp.h>
+#include <sys/types.h>
+void crashtest_3()
+{
+    #pragma omp parallel for
+    for (int i = 0; i < 4; i++)
+    {
+        printf("My Tid is % 8d\n", gettid());
+        int a = 1/0;
+    }
+}
+void crashtest_2()
+{
+    crashtest_3();
+}
+void crashtest_1()
+{
+    crashtest_2();
+}
+int main()
+{
+    crashtest_3();
+    // printf(NONE"Test\n"NONE);
+    // printf(LIGHT_BLUE"Test\n"NONE);
+    // printf(LIGHT_CYAN"Test\n"NONE);
+    // printf(LIGHT_GRAY"Test\n"NONE);
+    // printf(LIGHT_GREEN"Test\n"NONE);
+    // printf(LIGHT_PURPLE"Test\n"NONE);
+    // printf(LIGHT_RED"Test\n"NONE);
+    // printf(RED"Test\n"NONE);
+    // printf(GREEN"Test\n"NONE);
+    // printf(DARY_GRAY"Test\n"NONE);
+    // printf(BLUE"Test\n"NONE);
+    // printf(CYAN"Test\n"NONE);
+    // printf(PURPLE"Test\n"NONE);
+    // printf(BROWN"Test\n"NONE);
+    // printf(YELLOW"Test\n"NONE);
+    // printf(WHITE"Test\n"NONE);
+    // printf("Test\n");
+    return 0;
+}
